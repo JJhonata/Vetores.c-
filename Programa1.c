fb676bac7bc7668eaa4e99349f63a167697b2c37
Jhonata Vieira <astayunocorinthians@gmail.com>
2024-08-09 19:13:48 -0300

    Add files via upload

---
+++ Programa1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[6] = {1, 0, -2, -5, 7};
+    int soma = a[0] + a[1] + a[5];
+
+    printf("A soma dos valores serão:%d\n", soma);
+
+    a[4] = 100;
+
+    printf("Valores do array a:\n");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("a[%d]=%d\n", i, a[i]);
+    }
+    return 0;
+}
